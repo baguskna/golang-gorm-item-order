b89b970cb7a663526f3dcb11fc77c2a7e9c77c1e
Bagus Kurnia <bagus.kurnia@sirclo.com>
2022-11-24 21:26:56 +0700

    delete pw lol

--- app/db.go
+++ app/db.go
@@ -12,7 +12,7 @@ import (
 var DB *gorm.DB
 
 func StartDB() {
-	dsn := "root:ElxIvs2c@tcp(127.0.0.1:3306)/orders_by?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:@tcp(127.0.0.1:3306)/orders_by?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("DB connection error")
